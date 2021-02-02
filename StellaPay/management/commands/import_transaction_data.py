abe4ee52cf3055a29999f0c81cf5d3bbf52d5826
Vincent Bolta <mijnleraar@msn.com>
2021-02-02 20:40:38 +0100

    Make import method robust to all kinds of errors in the format.

--- StellaPay/management/commands/import_transaction_data.py
+++ StellaPay/management/commands/import_transaction_data.py
@@ -51,11 +51,47 @@ class Command(BaseCommand):
 
                 total_lines += 1
 
-                first_name, last_name = row['User'].split(" ", 1)
-                email_address = row['Email']
-                date_time = row['Date time']
-                item = row['Item bought']
-                transaction_price = row['Price']
+                full_name = row['User']
+
+                if " " in full_name:
+                    first_name, last_name = full_name.split(" ", 1)
+                else:
+                    first_name, last_name = "", ""
+
+                email_address = ""
+
+                if row['Email'] is not None:
+                    email_address = row['Email'].strip()
+
+                date_time = ""
+
+                if row['Date time'] is not None:
+                    date_time = row['Date time'].strip()
+
+                item = ""
+
+                if row['Item bought'] is not None:
+                    item = row['Item bought'].strip()
+
+                transaction_price = ""
+
+                if row['Price'] is not None:
+                    transaction_price = row['Price'].strip()
+
+                if email_address == "" and full_name == "":
+                    self.stderr.write(f"Could not import row {row} because it had no user AND no email address!")
+                    not_imported_rows.append(row)
+                    continue
+
+                if item == "":
+                    self.stderr.write(f"Ignoring the following row (since it has no product): {row}")
+                    not_imported_rows.append(row)
+                    continue
+
+                if transaction_price == "":
+                    self.stderr.write(f"Ignoring the following row (since it has no price): {transaction_price}")
+                    not_imported_rows.append(row)
+                    continue
 
                 # If we haven't loaded a reference to this product yet, we load it.
                 if item not in product_references:
@@ -67,23 +103,43 @@ class Command(BaseCommand):
                         not_imported_rows.append(row)  # Save this row because we failed to import it
                         continue
 
+                # User did not provide an email address, let's see if we can find the email address.
+                if email_address == "":
+                    user_match = Customer.objects.filter(first_name=first_name, last_name=last_name).first()
+
+                    if user_match is None:
+                        self.stderr.write(f"Could not find an email address for the user: {full_name}")
+                        not_imported_rows.append(row)
+                        continue
+
+                    email_address = user_match.email
+                    self.stdout.write(f"Performed reverse lookup to match {full_name} to {email_address}.")
+
                 # Load the users as a reference so we can use them as a foreign key
                 if email_address not in user_references:
                     try:
                         user_references[email_address] = Customer.objects.get(email=email_address)
                     except models.Customer.DoesNotExist:
-                        self.stderr.write(
-                            f"Could not this row because the user '{row['User']}' does not exist.")
-                        not_imported_rows.append(row)  # Save this row because we failed to import it
-                        continue
+                        # We could not find the user by email, but maybe if we look for a name instead
+                        user_match = Customer.objects.filter(first_name=first_name, last_name=last_name).first()
+
+                        if user_match is None:
+                            self.stderr.write(
+                                f"Could not import the following row because I could find not a corresponding user: '{row}'")
+                            not_imported_rows.append(row)  # Save this row because we failed to import it
+                            continue
+
+                        email_address = user_match.email
+                        self.stdout.write(f"Performed reverse lookup to match {full_name} to {email_address}.")
 
                 # Create object and store it in the database
                 Transaction.objects.create(buyer=user_references[email_address], item_bought=product_references[item],
                                            price=float(transaction_price))
 
-            # Provide feedback about how many were succesfully importend and how many were not.
+            # Provide feedback about how many were successfully imported and how many were not.
             self.stdout.write(
-                f"Read {total_lines} lines and successfully imported {total_lines - len(not_imported_rows)} lines.")
+                f"Read {total_lines} lines and successfully imported {total_lines - len(not_imported_rows)} "
+                f"({round(100 * ((total_lines - len(not_imported_rows)) / total_lines), 1)}%) lines.")
 
             # Check if we even have failed rows.
             if len(not_imported_rows) == 0:
